5de591ead071c8fd39ac3c8c752c400578e90304
lmea0005 <lmea0005@student.monash.edu>
2025-04-10 18:27:59 +1000

    Bug fix selection of RevealPicture component

--- src/components/RevealPicture.tsx
+++ src/components/RevealPicture.tsx
@@ -17,10 +17,9 @@ export default function RevealPicture({children}: RevealPictureProps) {
                 onMouseLeave={() => setHovered(false)}
             >
                 {children}
-                
             </div>
             <Image src="/images/henry.png" alt="Picture of me, Henry!" width={400} height={400}
-                className={`absolute duration-500 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50
+                className={`animate-float absolute pointer-events-none duration-500 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50
                 ${hovered ? "scale-100 opacity-100" : "scale-90 opacity-0"}`}
             />
         </>
